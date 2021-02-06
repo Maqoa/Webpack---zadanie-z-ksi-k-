9f74cdcebf9220c101a66a7d7b190cfc0be22703
Maciej Oleksiuk <maciej.oleksiuk@gmail.com>
2021-02-06 17:14:32 +0100

    z tabelą

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-class biblioteka {
-    constructor(autor, tytul, kategoria, priorytet) {
-        this.tytul = tytul;
-        this.autor = autor;
-        this.kategoria = kategoria;
-        this.priorytet = priorytet;
-    }
+'use strict';
+
+function dodaj() {
+    var tytul = document.getElementById("tytul").value;
+    var autor = document.getElementById("autor").value;
+    var kategoria = document.getElementById("kategoria").value;
+    var priorytet = document.getElementById("priorytet").value;
+
+    var nowywiersz = document.createElement("tr");
+    nowywiersz.innerHTML = "<td>" + tytul + "</td><td>" + autor + "</td><td>" + kategoria + "</td><td>" + priorytet + "</td>";
+    var dodanie = document.getElementById('bibliotekaksiazek');
+    dodanie.appendChild(nowywiersz);
 }
-var ksiazka = new biblioteka;
-var ksiazka = ['tytul', 'autor', 'kategoria', 'priorytet'];
-ksiazka[0] = document.getElementById("tytul").string;
-ksiazka[1] = document.getElementById("autor").string;
-ksiazka[2] = document.getElementById("kategoria").value;
-ksiazka[3] = document.getElementById("priorytet").value;
-
-console.log( ksiazka.join( " + " ) );  
-document.getElementById("send").onclick = biblioteka;
-
