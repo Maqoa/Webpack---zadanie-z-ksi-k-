1f0ac484ca1359214c327c90b6d9055a5ad1eeb4
Maciej Oleksiuk <maciej.oleksiuk@gmail.com>
2021-02-06 15:20:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
-
 class biblioteka {
-    constructor (autor, tytul, kategoria, priorytet){
-     
+    constructor(autor, tytul, kategoria, priorytet) {
         this.tytul = tytul;
-         this.autor = autor;
-     this.kategoria = kategoria;
-       this.priorytet = priorytet;
-    }
-    wyswietlInfo(){
-        console.log("tytul: " + this.tytul + "autor: "+ this.autor +  "kategoria: " + this.kategoria + "priorytet: " + this.priorytet);
+        this.autor = autor;
+        this.kategoria = kategoria;
+        this.priorytet = priorytet;
     }
 }
+var ksiazka = new biblioteka;
 var ksiazka = ['tytul', 'autor', 'kategoria', 'priorytet'];
-ksiazka[0] = document.getElementById("tytul").value;
-ksiazka[1] = document.getElementById("autor").value;
+ksiazka[0] = document.getElementById("tytul").string;
+ksiazka[1] = document.getElementById("autor").string;
 ksiazka[2] = document.getElementById("kategoria").value;
 ksiazka[3] = document.getElementById("priorytet").value;
 
-console.log( ksiazka[0] + ' i ' + ksiazka[1] + ' i ' + ksiazka[2] + ' i ' + ksiazka[3] );
-document.getElementById("send").onclick = ksiazka;
+console.log( ksiazka.join( " + " ) );  
+document.getElementById("send").onclick = biblioteka;
 
-
-   //     var ksiazka = ['autor', 'tytul', 'kategoria', 'priorytet'];
-   // }
-  //  wyswietlInfo() {
- //       console.log("autor: " + this.autor + ",  " + "tytul: " + this.tytul + "kategoria: " + this.kategoria + ",  " + "priorytet: " + this.priorytet);
-//    }
-//}
-//var tytulKs = document.getElementById("tytul").value;
-//var autorKs = document.getElementById("autor").value;
-//var autorKs = document.getElementById("kategoria").value;
-//var priorytetKs = document.getElementById("priorytet").value;
-//}
-//document.getElementById("send").onclick = bibliotekaKS;
