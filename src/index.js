7b9882aa286bfa46152042c8e0a805054fcbc8d4
Maciej Oleksiuk <maciej.oleksiuk@gmail.com>
2021-02-07 22:54:37 +0100

    poprawki bez kompilacji

--- src/index.js
+++ src/index.js
@@ -2,23 +2,34 @@
 var id = 0;
 var counter = 0;
 
-function dodaj() {
-    var tytul = document.getElementById("tytul").value;
+function addRow() {
+    var title  = document.getElementById("title").value;
     var autor = document.getElementById("autor").value;
-    var kategoria = document.getElementById("kategoria").value;
-    var priorytet = document.getElementById("priorytet").value;
-    var nowywiersz = document.createElement("tr");
-    nowywiersz.id = ++id;
-    nowywiersz.innerHTML = "<td>" + tytul + "</td><td>" + autor + "</td><td>" + kategoria + "</td><td>" + priorytet + "</td>" + "<td>" + "<button onclick=deleteRow(" + id + ");>usuń</button>" + "</td>";
-    var dodanie = document.getElementById('bibliotekaksiazek');
-    dodanie.appendChild(nowywiersz);
+    var category = document.getElementById("category").value;
+    var priority  = document.getElementById("priority ").value;
+    var newRow = document.createElement("tr");
+    newRow.id = ++id;
+    newRow.innerHTML = "<td>" + title  + "</td><td>" + autor + "</td><td>" + category + "</td><td>" + priority  + "</td>" + "<td>" + "<button onclick=deleteRow(" + id + ");>usuń</button>" + "</td>";
+    var rowAdd = document.getElementById('library');
+    rowAdd.appendChild(newRow);
     counter++;
     container.textContent = counter;
 }
 
 function deleteRow(id) {
-    var myobj = document.getElementById(id);
-    myobj.remove();
+    var rowToDelete = document.getElementById(id);
+    rowToDelete.remove();
     counter--;
     container.textContent = counter;
+}
+
+
+function addCategory(){
+    var categoryName = document.getElementById("newCategory").value;
+    var categories = document.getElementById("category");
+    var newCategory = document.createElement("option");
+    newCategory.value = categoryName;
+    newCategory.text = categoryName;
+    categories.appendChild(newCategory);
+   
 }
