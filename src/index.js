6989bdda27f92d4f07cab0a21fecbddcea3efbf4
Maciej Oleksiuk <maciej.oleksiuk@gmail.com>
2021-02-07 20:44:36 +0100

    zadanie bez kompilacji

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 'use strict';
+var id = 0;
+var counter = 0;
 
 function dodaj() {
     var tytul = document.getElementById("tytul").value;
     var autor = document.getElementById("autor").value;
     var kategoria = document.getElementById("kategoria").value;
     var priorytet = document.getElementById("priorytet").value;
-
-
     var nowywiersz = document.createElement("tr");
-
-    
-    nowywiersz.innerHTML = "<td>" + tytul + "</td><td>" + autor + "</td><td>" + kategoria + "</td><td>" + priorytet + "</td>" + "<td>" + "<button>" + "</td>";
+    nowywiersz.id = ++id;
+    nowywiersz.innerHTML = "<td>" + tytul + "</td><td>" + autor + "</td><td>" + kategoria + "</td><td>" + priorytet + "</td>" + "<td>" + "<button onclick=deleteRow(" + id + ");>usuń</button>" + "</td>";
     var dodanie = document.getElementById('bibliotekaksiazek');
     dodanie.appendChild(nowywiersz);
-
+    counter++;
+    container.textContent = counter;
 }
 
+function deleteRow(id) {
+    var myobj = document.getElementById(id);
+    myobj.remove();
+    counter--;
+    container.textContent = counter;
+}
